80a86eae378c4ca6c36bae0fc5d55c7fa24deeea
Kieran Simpson <kierans777@gmail.com>
2016-06-03 10:20:26 +1000

    Updated to dedupe a package's modules before shrinkwrapping.

--- tasks/shrinkwrap.js
+++ tasks/shrinkwrap.js
@@ -10,14 +10,30 @@
 
 var shelljs = require('shelljs');
 
+function exec(grunt, cmd, errorMsg) {
+  var result = shelljs.exec(cmd);
+
+  if (result.code !== 0) {
+    grunt.fatal(errorMsg);
+  }
+}
+
+function shrinkwrap(grunt) {
+  exec(grunt, 'npm shrinkwrap --production', 'shrinkwrapping failed.');
+}
+
+function dedupe(grunt) {
+  exec(grunt, 'npm dedupe', 'dedupe failed.');
+}
+
 module.exports = function(grunt) {
-
   grunt.registerTask('shrinkwrap', 'Grunt task for shrinkwrapping your projects dependencies via npm shrinkwrap', function() {
-    var result = shelljs.exec('npm shrinkwrap --production');
+    var config = grunt.config('shrinkwrap');
 
-    if (result.code !== 0) {
-      grunt.fatal("shrinkwrapping failed.");
+    if (config.dedupe) {
+      dedupe(grunt);
     }
-  });
 
+    shrinkwrap(grunt);
+  });
 };
